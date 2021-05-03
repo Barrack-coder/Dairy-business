1a06ad979b6c34a73e892fbaa53434e28f6b2063
Barrack-coder <barrack.juma@student.moringaschool.com>
2021-05-03 08:46:44 +0300

    added files

---
+++ index.js
@@ -0,0 +1,16 @@
+//Global variable   
+//logging somethig in the console
+//this is my first javascript code
+let selling_price = 45; //cost of milk per litre
+let shedsProduction = [510, 308, 486, 572];
+let shedsName = ["1",
+    "2",
+    "3",
+    "4"
+]
+
+//initial value of litres
+let totalLitres = 0;
+
+let week = 7;
+let year = 365;
