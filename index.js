5090bdf29bca22478e87798b358f3f07319f7267
Barrack-coder <barrack.juma@student.moringaschool.com>
2021-05-04 05:18:05 +0300

    added changes in index.js

--- index.js
+++ index.js
@@ -4,3 +4,20 @@
 let selling_price = 45; //cost of milk per litre
 let shedsProduction = [510, 308, 486, 572];
 let shedsName = ["1", "2", "3", "4"]
+var sheds = [{
+        name: "shed 1",
+        averageProduction: 510,
+    },
+    {
+        name: "shed 2",
+        averageProduction: 308,
+    },
+    {
+        name: "shed 3",
+        averageProduction: 486,
+    },
+    {
+        name: "shed 4",
+        averageProduction: 572,
+    },
+];
