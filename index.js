46f13d0a4b8420692bd1994eef76dac7b2740974
Barrack-coder <barrack.juma@student.moringaschool.com>
2021-05-04 05:11:04 +0300

    added changes in index.js and index.html

--- index.js
+++ index.js
@@ -3,14 +3,4 @@
 //this is my first javascript code
 let selling_price = 45; //cost of milk per litre
 let shedsProduction = [510, 308, 486, 572];
-let shedsName = ["1",
-    "2",
-    "3",
-    "4"
-]
-
-//initial value of litres
-let totalLitres = 0;
-
-let week = 7;
-let year = 365;
+let shedsName = ["1", "2", "3", "4"]
